d3b1b7f04a6dbebfbff00ec61b61f4786e713f5c
beb0 <abd.diaa.g@gmail.com>
2025-02-22 18:32:44 +0100

    structure for generating transactions and entities

---
+++ generator/data_fill.py
@@ -0,0 +1,12 @@
+import requests
+# from faker import faker
+
+# should fill the entities here (entities, accounts, etc..)
+
+# res = requests.get('https://randomuser.me/api/')
+# print(res.json())
+
+
+
+
+
---
+++ generator/generator.py
@@ -0,0 +1,16 @@
+import random
+from uuid import uuid1
+from datetime import  datetime
+
+class Generator:
+
+    cities = ['New York', 'Tokyo', 'Barcelona', 'Cairo', 'Berlin', 'Monza']
+
+    def transaction_generator(self):
+        return {
+            'user_id' : random.randint(1,100),
+            'transaction_id': str(uuid1()),
+            'date_time': datetime.now().isoformat(),
+            'location': random.choice(self.cities),
+            'amount' : round(random.uniform(1,1000000), 2)
+        }
